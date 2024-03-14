8357e80936eb69ef59f81a1710ea6cfcaf46d2c6
Jiatong Li <102882840+Jiatonglii22@users.noreply.github.com>
2024-03-14 16:12:53 -0400

    largest query

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -26,32 +26,40 @@ export default function QueryProcessor(query: string): any {
     );
   };
 
-  // if (query.toLowerCase().includes("Which of the following numbers is the largest:")) {
-  //   var qlist:string[] = query.split('');
-  //   var num1:number = Number(qlist[9].slice(0, -1)); 
-  //   var num2:number = Number(qlist[10].slice(0, -1));
-  //   var num3:number = Number(qlist[11].slice(0, -1));
-  //   var largest:number = num1;
-  //   if (num1 > num2) {
-  //     if (num1 > num3) {
-  //       largest = num1;
-  //     }
-  //     else {
-  //       largest = num3;
-  //     }
-  //   }
-  //   if (num2 > num3) {
-  //     if (num2 > num1) {
-  //       largest = num2 
-  //     }
-  //     else {
-  //       largest = num1
-  //     }
-  //   }
-  //   return ( 
-  //     largest.toString()
-  //   );
-  // };
+  if (query.toLowerCase().includes("Which of the following numbers is the largest:")) {
+    var qlist:string[] = query.split('');
+    var num1:number = Number(qlist[9].slice(0, -1)); 
+    var num2:number = Number(qlist[10].slice(0, -1));
+    var num3:number = Number(qlist[11].slice(0, -1));
+    var largest:number = num1;
+    if (num1 > num2) {
+      if (num1 > num3) {
+        largest = num1;
+      }
+      else {
+        largest = num3;
+      }
+    }
+    if (num2 > num3) {
+      if (num2 > num1) {
+        largest = num2 
+      }
+      else {
+        largest = num1
+      }
+    }
+    if (num3 > num2) {
+      if (num3 > num1) {
+        largest = num3 
+      }
+      else {
+        largest = num1
+      }
+    }
+    return ( 
+      largest
+    );
+  };
 
   //Which of the following numbers is the largest: 96, 88, 46?
 
